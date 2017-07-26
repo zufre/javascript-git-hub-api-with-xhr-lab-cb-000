79e16819da57c175b377fb42dcac32059ff120da
Gabe Jackson <gj@mail.co.de>
2017-07-26 14:39:59 -0400

    Update test octocat repo
    Test XHRs faked by Sinon anyway, but this is just in case students go peeking around in the test file

--- test/index-test.js
+++ test/index-test.js
@@ -46,7 +46,7 @@ describe('index', () => {
     let el
 
     before(() => {
-      el = { dataset: { repository: 'test-repo', username: 'octocat' } }
+      el = { dataset: { repository: 'Spoon-Knife', username: 'octocat' } }
       xhr = sinon.useFakeXMLHttpRequest()
       window.XMLHttpRequest = xhr
 
@@ -77,7 +77,7 @@ describe('index', () => {
       it('calls out to Github', () => {
         getCommits(el)
         expect(requests.length).toBe(1)
-        expect(requests[0].url).toBe('https://api.github.com/repos/octocat/test-repo/commits')
+        expect(requests[0].url).toBe('https://api.github.com/repos/octocat/Spoon-Knife/commits')
       })
     })
 
@@ -85,7 +85,7 @@ describe('index', () => {
       it('calls out to Github', () => {
         getBranches(el)
         expect(requests.length).toBe(1)
-        expect(requests[0].url).toBe('https://api.github.com/repos/octocat/test-repo/branches')
+        expect(requests[0].url).toBe('https://api.github.com/repos/octocat/Spoon-Knife/branches')
       })
     })
 
